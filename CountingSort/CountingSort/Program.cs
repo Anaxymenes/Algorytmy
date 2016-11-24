7675c079acc1d11018d6749df1e40c98c34ec8bb
Sonar <Sonar>
2016-11-24 13:11:06 +0100

    CountingSort i SelectionSort

---
+++ CountingSort/CountingSort/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CountingSort
+{
+    class Program
+    {
+        private string WyswietlElementyTablicy(int[] a)
+        {
+            string txt = "";
+            for (int i = 0; i < a.Length; i++)
+                txt += a[i] + " ";
+            return txt;
+        }
+        static void Main(string[] args)
+        {
+            Program wyswietl = new Program();
+            int[] tab = {6,3,6,1,4,9,0,1,8,2,6,4,9,3,7,5,9,2,7,3,2,4,1,8,7,0,8,5,8,3,6,2,5,3};
+            Console.WriteLine("Tablica nie posortowana:\n"+wyswietl.WyswietlElementyTablicy(tab));
+            int max = 9+1; //max element tablicy
+            int[] temp = new int[max];
+            //zerujemy tablice temp
+            for (int i = 0; i < temp.Length; i++)
+                temp[i] = 0;
+            // zliczanie ile razy wystepuje dana liczba
+            for (int i = 0; i < tab.Length; i++)
+            {
+                //Console.Write("Dla wartosciL: {0}, zwiekszamy z wartości: {1} na wartość: ",tab[i],temp[tab[i]]);
+                temp[tab[i]]++;
+                //Console.WriteLine(temp[tab[i]].ToString());
+            }
+            Console.WriteLine("Po zliczeniu: \n"+wyswietl.WyswietlElementyTablicy(temp));
+
+            //Sumowanie wartości
+            for (int i = 1; i < temp.Length; i++)
+                temp[i] += temp[i - 1];
+            Console.WriteLine("Po sumowaniu: \n" + wyswietl.WyswietlElementyTablicy(temp));
+
+            //sortowanie
+            int[] b = new int[tab.Length];
+            //Console.WriteLine(b.Length + " <- b || tab -> " + tab.Length);
+            for (int i = 0; i < tab.Length; i++)
+            { 
+                b[temp[tab[i]]-1] = tab[i];
+                temp[tab[i]] -= 1;
+            }
+            Console.WriteLine("Po sortowaniu: \n" + wyswietl.WyswietlElementyTablicy(b));
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ SelectionSort/SelectionSort/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        private void WyswietlTablice(int[] tab)
+        {
+            for (int i = 0; i < tab.Length; i++)
+                Console.Write(tab[i] + " ");
+            Console.WriteLine();
+        }
+        private void SelectionSort(int[] a)
+        {
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < a.Length; j++)
+                {
+                    if (a[min] > a[j])
+                        min = j;
+                }
+                int temp = a[i];
+                a[i] = a[min];
+                a[min] = temp;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Program program = new Program();
+            //int[] a = { 3, 2, 7, 4, 1, 9, 5, 3, 1, 0 };
+            int n = 10; // zmienna odpowiedzialna za wielkosc tablicy
+            int rMin = 0, rMax = 9; //zmienne odpowiedzialne za zakres losowania liczb
+            int[] a = new int[n];
+            //generowanie liczb i wstawienie do tablicy
+            for(int i=0;i<n;i++)
+                a[i] = rand.Next(rMin,rMax);
+            //wyswietlenie tablicy
+            Console.WriteLine("Tablica nieposortowana");
+            program.WyswietlTablice(a);
+            //sortowanie
+            program.SelectionSort(a);
+            //Wyswietlenie posortowanej tablicy
+            Console.WriteLine("Tablica po sortowaniu");
+            program.WyswietlTablice(a);
+            Console.ReadKey();
+        }
+    }
+}
